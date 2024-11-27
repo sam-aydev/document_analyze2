98f271ef0a72df4ffb774fb987cfe3e9da580b5d
Adetunji Samuel <samadet0001@gmail.com>
2024-11-27 10:45:53 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    BlogModule,
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        autoLoadEntities: true,
+        port: 5432,
+        username: 'postgres',
+        password: '12345',
+        host: 'localhost',
+        database: 'document_nestjs',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
